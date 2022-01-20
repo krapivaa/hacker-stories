b44fcc2a3ed062924aad3ffd69caab6f3a594abc
Marina Iv <marisha@marinas-mbp.myfiosgateway.com>
2022-01-19 19:11:25 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import * as React from 'react';
+import axios from 'axios';
 // import './App.css';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(
-      () => resolve({ data: { stories: initialStories } }),
-      2000
-    )
-  );
+// const initialStories = [
+//   {
+//     title: 'React',
+//     url: 'https://reactjs.org/',
+//     author: 'Jordan Walke',
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0,
+//   },
+//   {
+//     title: 'Redux',
+//     url: 'https://redux.js.org/',
+//     author: 'Dan Abramov, Andrew Clark',
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1,
+//   },
+// ];
+
+// const getAsyncStories = () =>
+//   new Promise((resolve) =>
+//     setTimeout(
+//       () => resolve({ data: { stories: initialStories } }),
+//       2000
+//     )
+//   );
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -80,26 +83,53 @@ const App = () =>  {
     'React'
   );
 
+  const [url, setUrl] = React.useState(
+    `${API_ENDPOINT}${searchTerm}`
+  );
+
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer,
     { data: [], isLoading: false, isError: false }
   );
   
 
-  React.useEffect(() => {
+  const handleFetchStories = React.useCallback(async () => {
+
+    // if (!searchTerm) return;
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
  
-    getAsyncStories()
-    .then((result) => {
+    // getAsyncStories()
+    // fetch(url) 
+    //   .then((response) => response.json()) 
+    // axios
+    //   .get(url)
+    //   .then((result) => {
+    //     dispatchStories({
+    //       type: 'STORIES_FETCH_SUCCESS',
+    //       // payload: result.data.stories,
+    //       // payload: result.hits,
+    //       payload: result.data.hits,
+    //     });
+    //   })
+    //   .catch(() => 
+    //     dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+    //   );
+    // }, [url]);
+    try {
+      const result = await axios.get(url);
+
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.stories,
+        payload: result.data.hits,
       });
-    })
-    .catch(() => 
-      dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-    );
-  }, []);
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+    }
+  }, [url]);
+  
+  React.useEffect(() => {
+    handleFetchStories(); 
+  }, [handleFetchStories]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -108,14 +138,20 @@ const App = () =>  {
     });
   };
 
-  const handleSearch = (event) => {
+  const handleSearchInput = (event) => {
     // console.log(event.target.value);
     setSearchTerm(event.target.value);
   };
 
-  const searchedStories = stories.data.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const handleSearchSubmit = (event) => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+    event.preventDefault();
+  };
+
+  // const searchedStories = stories.data.filter((story) =>
+  //   story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  // );
 
   return (
       <div className="App">
@@ -123,17 +159,11 @@ const App = () =>  {
       {/* <h1>Hello {getTitle('React')}</h1> */}
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel
-        id="search"
-        label="Search"
-        value={searchTerm}
-        isFocused
-        onInputChange={handleSearch}
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
-
-      {/* <Search search={searchTerm} onSearch={handleSearch} /> */}
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       <hr />
 
@@ -142,11 +172,33 @@ const App = () =>  {
       {stories.isLoading ? (
         <p>Loading ...</p>
         ) : (
-          <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
+          // <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
+          <List list={stories.data} onRemoveItem={handleRemoveStory} />
         )}
       </div>
     );
 };
+
+  const SearchForm = ({
+    searchTerm,
+    onSearchInput,
+    onSearchSubmit,
+  }) => (
+    <form onSubmit={onSearchSubmit}>
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={onSearchInput}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
+
+      <button type="submit" disabled={!searchTerm}>
+        Submit
+      </button>
+    </form>
+  );
 
   const InputWithLabel = ({ 
     id,
